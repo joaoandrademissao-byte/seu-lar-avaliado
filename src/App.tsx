092ad4d136943d845faa1aad4b2100c9f5119f0f
joaoandrademissao-byte <joaoandrademissao@gmail.com>
2026-03-15 17:55:33 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Obrigado from "./pages/Obrigado";
 import PoliticaPrivacidade from "./pages/PoliticaPrivacidade";
+import { WhatsAppModalProvider } from "@/contexts/WhatsAppModalContext";
+import WhatsAppModal from "@/components/landing/WhatsAppModal";
 import { useEffect } from "react";
 
 const queryClient = new QueryClient();
@@ -30,13 +32,16 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/obrigado" element={<Obrigado />} />
-            <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <WhatsAppModalProvider>
+            <WhatsAppModal />
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/obrigado" element={<Obrigado />} />
+              <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </WhatsAppModalProvider>
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
